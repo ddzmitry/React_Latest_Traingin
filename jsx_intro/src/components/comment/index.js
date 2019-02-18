c750b13103609a74fca26f4a3a3d78581bc2c2f4
Dzmitry Dubarau <ddzmitry@yahoo.com>
2019-02-18 11:18:33 -0500

    More on React

--- jsx_intro/src/components/comment/index.js
+++ jsx_intro/src/components/comment/index.js
@@ -1,41 +1,31 @@
-import React, { Component } from 'react';
+import React from "react";
 
-class Comment extends Component {
-    constructor(props) {
-        super(props);
-        let {author,time,comment,date,imgUrl} = props;
-          this.state = {  
-            author,
-            time,
-            comment,
-            date,
-            imgUrl
-        };
-    }
-        
-    render() { 
-      let {author,time,comment,date,imgUrl} = this.state;
-        return (         
-      <div className="comment">
-        <a className="avatar">
-              <img src={imgUrl} />
+const Comment = props => {
+  let { author, time, comment, date, imgUrl } = props;
+  return (
+    <div className="comment">
+      <a href="/" className="avatar">
+        <img alt={date} src={imgUrl} />
+      </a>
+      <div className="content">
+        <a href="/" className="author">
+          {author}
         </a>
-        <div className="content">
-              <a className="author">{author}</a>
         <div className="metadata">
-              <span className="date">{time}</span>
+          {" "}
+          <span className="date">{time}</span>
         </div>
-              <div className="text">
-                {comment}
+        <div className="text"> {comment}</div>
+        <div className="actions">
+          {" "}
+          <a href="/" className="reply">
+            Reply
+          </a>
         </div>
-              <div className="actions">
-        <a className="reply">Reply</a>
-              </div>
-            </div>
-      </div>
+      </div>{" "}
+    </div>
+  );
 
-         );
-    }
-}
- 
+};
+
 export default Comment;
--- jsx_intro/src/index.js
+++ jsx_intro/src/index.js
@@ -1,46 +1,45 @@
 // Import the React and ReactDOM Libraried
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Comment from './components/comment';
-import faker from 'faker';
+import React from "react";
+import ReactDOM from "react-dom";
+import Comment from "./components/comment";
+import ApprovalCard from "./components/approvalcard";
+
+import faker from "faker";
 let today = new Date();
-let getTime = () => today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-let getDate =  () => today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 
-let data = [
-];
 
-let getuser = () => (
-    {  
-        author: faker.internet.userName,
-        time: getTime(),
-        comment: faker.random.words(),
-        date: getDate(),
-        imgUrl: faker.image.avatar()
-    }
-);
+let getTime = () =>
+  today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+let getDate = () =>
+  today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+
+let data = [];
+
+let getuser = () => ({
+  author: faker.internet.userName(),
+  time: getTime(),
+  comment: faker.random.words(),
+  date: getDate(),
+  imgUrl: faker.image.avatar()
+});
 for (let i = 0; i < 10; i++) {
-    data.push(getuser());
+  data.push(getuser());
 }
 // Create React Component
-const App = () => { 
-return (
-          <div >
-          <div className="ui comments">
-          
-          {data.map((props_data,i) => {
-                return <Comment key={i} {...props_data} />;
-          })}
-          </div>
-
-          </div>
-      );
+const App = () => {
+  return (
+      <div className = "ui container  comments">
+        {data.map((props_data, i) => {
+          return (
+          <ApprovalCard key={i}>
+                <Comment {...props_data} />
+          </ApprovalCard>
+          )
+        })
+        }
+      </div>
+  );
 };
 
-
-
 // Take the React Component and show it in the screen
-ReactDOM.render(
-    <App />,
-    document.querySelector("#root")
-);
+ReactDOM.render(<App />, document.querySelector("#root"));

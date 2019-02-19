a580fd6b7a8c46b6d75927374335f692a8a6cec8
Dzmitry Dubarau <ddzmitry@yahoo.com>
2019-02-19 18:05:30 -0500

    More on React

--- class_based_components_demo/src/components/seasonDisplay/index.jsx
+++ class_based_components_demo/src/components/seasonDisplay/index.jsx
@@ -1,10 +1,26 @@
 
 import React, { Component } from 'react';
 
+const getSeason = (lat, month) => {
+    console.log(month)
+    if(month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+
+}
 class seasonDisplay extends Component {
+
     render(props) {
+        const season = getSeason(this.props.lat,new Date().getMonth());
+        const icon = season === 'winter' ? 'snowflake icon' : 'sun icon';
         return (
             <div>
+                <i className={`${icon}`}></i>
+                
+                {season === 'winter' ? 'brr chilly'   : 'lets hit the beach <i class="sun icon"></i>'}
+                <br />
                 {this.props.waiting ? "Waiting on Fetch" : this.props.lat ? `Lattiture: ${this.props.lat}` : `Error: ${this.props.error}`}
                 <br />
                 {this.props.waiting ? "Waiting on Fetch" : this.props.long ? `Longitude: ${this.props.lat}` : `Error: ${this.props.error}`}

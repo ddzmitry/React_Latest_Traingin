5ba377bfb87081a6699094f1f32414c6ded70a1d
Dzmitry Dubarau <dzmitry.dubarau@gmail.com>
2019-03-24 18:10:41 -0400

    More on React

--- streams/client/src/components/Streams/StreamCreate.js
+++ streams/client/src/components/Streams/StreamCreate.js
@@ -1,26 +1,43 @@
 
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions/index';
 class StreamCreate extends React.Component {
-    renderInput(formProps) {
-        const { input, label } = formProps
-        console.log(input)
+
+    renderErrors = ({ error, touched }) => {
+        if (touched && error) {
+            return (
+                <div className="ui error message">
+                    <div className="header">{error}</div>
+                </div>
+            );
+        }
+    }
+
+    renderInput = (formProps) => {
+        const { input, label, meta } = formProps
+        const classname = `field ${meta.error && meta.touched ? 'error' : ''}`;
+        // console.log(input)
+        // Meta will have errors key
+        // console.log('meta', meta)
         // adding input properties
         // like on change and etc.
         return (
-            <div className="field">
+            <div className={classname}>
                 <label>{label}</label>
-                <input {...input} />
+                <input {...input} autoComplete="off" />
+                {this.renderErrors(meta)}
             </div>
         )
     }
 
-    onSubmit(formValues) {
-        console.log(formValues)
+    onSubmit = (formValues) => {
+        this.props.createStream(formValues)
     }
     render() {
         return (
-            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
                 <Field name="title" component={this.renderInput} label="Enter Title" />
                 <Field name="description" component={this.renderInput} label="Enter Description" />
                 <button className="ui button primary">Submit</button>
@@ -28,8 +45,25 @@ class StreamCreate extends React.Component {
         )
     }
 };
+const validate = (formValues) => {
 
-export default reduxForm({
-    form: 'streamCreate'
+    const errors = {}
+    if (!formValues.title) {
+        errors.title = "Need Title";
+    }
+    else if (!formValues.description) {
+        errors.description = "Need description";
+    }
+    return errors;
+
+}
+
+const formWrapped = reduxForm({
+    form: 'streamCreate',
+    validate
 })(StreamCreate);
+
+
+
+export default connect(null,{createStream})(formWrapped);
 
--- streams/client/src/components/Streams/StreamList.js
+++ streams/client/src/components/Streams/StreamList.js
@@ -1,8 +1,80 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStreams } from '../../actions';
+import { Link } from 'react-router-dom';
+class StreamList extends React.Component {
 
-const StreamList = () => {
+    componentDidMount = () => {
+        this.props.fetchStreams();
+    }
+    renderAdmin = (stream) => {
+        try {
+            if (stream.userId === this.props.currentUserId) {
+                return (
+                    <div className="right floated content">
+                        <button className="ui button primary">Edit</button>
+                        <button className="ui button negative">Delete</button>
+                    </div>
+                )
+            }
+        } catch (error) {
 
-    return <div>StreamList</div>;
+
+        }
+
+
+    }
+    renderList() {
+        return this.props.streams.map(stream => {
+            this.renderAdmin()
+            return (
+                <div className='item' key={stream.id}>
+                    {this.renderAdmin(stream)}
+                    <i className="large middle aligned icon camera" />
+                    <div className="content">
+                        {stream.title}
+                        <div className="description">
+                            {stream.description}
+                        </div>
+                    </div>
+                </div>
+            );
+        })
+    }
+
+    renderCreate() {
+        if (this.props.isSignedIn) {
+            return (
+                <div style={{textAlign:'right'}}>
+                    <Link to='/streams/new' className="ui button primary">
+                        Create Stream
+                        </Link>
+                </div>
+            )
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Streams</h2>
+                <div className="ui celled list">
+                    {this.renderList()}
+                    {this.renderCreate()}
+                </div>
+            </div>
+        )
+    }
+
 };
 
-export default StreamList;
+const mapStateToProps = (state, prevState) => {
+
+    return {
+        streams: [...Object.values(state.streams)],
+        currentUserId: state.authReducer.userId,
+        isSignedIn: state.authReducer.isSignedIn
+    }
+}
+
+export default connect(mapStateToProps, { fetchStreams })(StreamList);

8c297db502069b8ff3741218ef2f04aad8c0953c
Dzmitry Dubarau <dzmitry.dubarau@gmail.com>
2019-03-27 20:02:31 -0400

    Added Switch

--- streams/client/src/components/Streams/StreamShow.js
+++ streams/client/src/components/Streams/StreamShow.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { fetchStream } from '../../actions'
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const StreamShow = () => {
 
     return <div>StreamShow</div>;
 };
+
+const mapStateToProps = () => {
+    
+}
 
 export default StreamShow;

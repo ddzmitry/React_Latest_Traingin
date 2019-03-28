5a534df6354b2d9d5e83e98cd8b61c0d05d0d76b
Dzmitry Dubarau <dzmitry.dubarau@gmail.com>
2019-03-27 20:14:38 -0400

    Video App Refactor

--- streams/client/src/components/Streams/StreamShow.js
+++ streams/client/src/components/Streams/StreamShow.js
@@ -3,13 +3,33 @@ import { fetchStream } from '../../actions'
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-const StreamShow = () => {
+class StreamShow extends React.Component {
 
-    return <div>StreamShow</div>;
-};
+    componentDidMount = () => {
 
-const mapStateToProps = () => {
-    
+        this.props.fetchStream(this.props.match.params.id)
+    }
+
+    render() {
+        if (!this.props.stream) {
+            return <div>Loading</div>
+        }
+        const { title, description } = this.props.stream;
+
+        return (
+            <div>
+                <h1>{title}</h1>
+                <h5>{description}</h5>
+            </div>
+        )
+    }
+
+}
+const mapStateToProps = (state, ownState) => {
+    return {
+        stream: state.streams[ownState.match.params.id],
+    }
+
 }
 
-export default StreamShow;
+export default connect(mapStateToProps, { fetchStream })(StreamShow)

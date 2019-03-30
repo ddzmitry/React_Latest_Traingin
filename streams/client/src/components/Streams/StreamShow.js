a9304e83144a1e140e4851ef89a46035a31dc527
Dzmitry Dubarau <dzmitry.dubarau@gmail.com>
2019-03-30 19:00:12 -0400

    Video app finished

--- streams/client/src/components/Streams/StreamShow.js
+++ streams/client/src/components/Streams/StreamShow.js
@@ -1,14 +1,44 @@
 import React from 'react';
 import { fetchStream } from '../../actions'
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import flv from 'flv.js';
+
 // videoapp
-
 class StreamShow extends React.Component {
+    constructor(props) {
+        super(props)
+        this.videoRef = React.createRef();
 
+    }
     componentDidMount = () => {
+        const { id } = this.props.match.params
+        this.props.fetchStream(id)
+        this.buildPlayer()
 
-        this.props.fetchStream(this.props.match.params.id)
+    }
+
+    componentDidUpdate = (prevProps, prevState) => {
+        this.buildPlayer()
+    }
+
+    componentWillUnmount = () => {
+      this.player.destroy()
+    }
+    
+    buildPlayer() {
+
+
+        if (this.player || !this.props.stream) {
+            return;
+        }
+        const { id } = this.props.match.params
+
+        this.player = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${id}.flv`
+        })
+        this.player.attachMediaElement(this.videoRef.current)
+        this.player.load()
     }
 
     render() {
@@ -19,7 +49,10 @@ class StreamShow extends React.Component {
 
         return (
             <div>
+                <video ref={this.videoRef} style={{ width: '100%' }} controls={true} />
+
                 <h1>{title}</h1>
+
                 <h5>{description}</h5>
             </div>
         )

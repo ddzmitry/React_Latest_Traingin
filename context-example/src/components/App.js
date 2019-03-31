00e537f5b5d73bb5fb0a2ae12b81920eab14c757
Dzmitry Dubarau <dzmitry.dubarau@gmail.com>
2019-03-31 13:04:31 -0400

    More on OCntext

---
+++ context-example/src/components/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import UserCreate from './userCreate'
+import LanguageContext from '../context/LanguageContext';
+
+export default class App extends Component {
+
+    state = { language: 'english' }
+
+    onLanguageChange = (language) => {
+        this.setState({ language })
+    }
+    render() {
+        return (
+            <div className="ui container">
+                <div>
+                    Select a language:
+            <i onClick={() => this.onLanguageChange('english')} className="flag us " />
+                    <i onClick={() => this.onLanguageChange('dutch')} className="flag nl " />
+                </div>
+                <LanguageContext.Provider value={this.state.language}>
+                <UserCreate />
+                </LanguageContext.Provider>
+            </div>
+        )
+    }
+}
---
+++ context-example/src/components/Button.js
@@ -0,0 +1,12 @@
+
+import React, { Component } from 'react'
+import LanguageContext from '../context/LanguageContext';
+export default class Button extends Component {
+  static contextType = LanguageContext;
+  render() {
+      const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
+    return (
+        <button className="ui button primary"> {text} </button>        
+    )
+  }
+}
